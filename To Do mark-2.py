fb72f87e682a2379880d14b372b34ee67b6f93fa
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-03-17 18:04:12 +0900

    add some funcitons

--- To Do mark-2.py
+++ To Do mark-2.py
@@ -15,7 +15,9 @@ def adding_tasks(list_of_tasks,new_tasks,due_task):
 #Function: Append due day and time
 
 #Function:Append priority of tasks
-
+#function: remove particular tasks
+def finished_tasks(list_of_tasks, complete_task=None):
+    list_of_tasks.pop(complete_task)
 #Function:Remove tasks from lists
 def clear_tasks(list_of_tasks):
     list_of_tasks.clear()
@@ -28,7 +30,8 @@ def main_todo():
     while True:
         print("1: display tasks")
         print("2: add tasks")
-        print("3: add due day")
+        print("3: remove tasks")
+        print("4: delete them all")
 
         num_ope_tasks = input("Please input number: ")
 
@@ -39,6 +42,9 @@ def main_todo():
             due_task = input("Enter due of task: ")
             adding_tasks(list_of_tasks, new_tasks,due_task)
         elif num_ope_tasks == "3":
+            complete_tasks = input("Please enter number: ")
+            finished_tasks(list_of_tasks,complete_tasks)
+        elif num_ope_tasks == "4":
             clear_tasks(list_of_tasks)
             break
         else:
---
+++ generate_code.py
@@ -0,0 +1,7 @@
+dt_nw = datetime.now()
+
+# to get hour from datetime
+print('Hour: ', dt_nw.hour)
+
+# to get minute from datetime
+print('Minute: ', dt_nw.minute)
